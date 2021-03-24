c116be303583da0fe3dfc44a221eee8d555ad626
Fernando Herrera <fernando.herrera85@gmail.com>
2021-03-24 15:25:56 -0400

    Antes del require cycle

--- ios/RutasApp/AppDelegate.m
+++ ios/RutasApp/AppDelegate.m
@@ -1,5 +1,7 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -27,6 +29,8 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDuWb0o8iX2ElgEjCk6YzozBNCvG3vMSRM"];
+
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+
 export const MapScreen = () => {
     return (
-        <View>
-            <Text>MapScreen</Text>
+        <View style={{ flex: 1 }}>
+            <MapView
+                style={{ flex: 1 }}
+                // provider={ PROVIDER_GOOGLE }
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
         </View>
     )
 }
